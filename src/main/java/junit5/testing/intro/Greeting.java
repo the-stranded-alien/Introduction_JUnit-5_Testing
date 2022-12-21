7a8ab06f80d68d9b7789b49d08c91b4de53b7352
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-21 14:16:52 +0530

    Testing with JUnit - 5

---
+++ src/main/java/junit5/testing/intro/Greeting.java
@@ -0,0 +1,10 @@
+package junit5.testing.intro;
+
+public class Greeting {
+
+    private static final String HELLO = "Hello";
+    private static final String WORLD = "World";
+
+    public String helloWorld() { return HELLO + " " + WORLD; }
+    public String helloWorld(String name) { return HELLO + " " + name; }
+}
---
+++ src/test/java/junit5/testing/intro/IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package junit5.testing.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
