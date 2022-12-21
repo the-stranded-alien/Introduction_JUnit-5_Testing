cf450223b7c6a4b9fda68ece8cdd0af96bdb53ae
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-21 14:37:41 +0530

    Before and After methods

--- src/test/java/junit5/testing/intro/GreetingTest.java
+++ src/test/java/junit5/testing/intro/GreetingTest.java
@@ -1,13 +1,17 @@
 package junit5.testing.intro;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called once ...");
+    }
     @BeforeEach
     void setUp() {
         System.out.println("In before each ...");
@@ -21,6 +25,21 @@ class GreetingTest {
 
     @Test
     void helloWorld1() {
-        System.out.println(greeting.helloWorld("Test"));
+        System.out.println(greeting.helloWorld("Test-1"));
+    }
+
+    @Test
+    void helloWorld12() {
+        System.out.println(greeting.helloWorld("Test-2"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each ...");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After - I am only called once ...");
     }
 }
